f658d8f0d2cefaa6db78072a6473190c71960e47
lulamae12 <tls.tommy11@gmail.com>
2023-09-02 13:04:51 -0400

    commit list output

--- scrapehistory.py
+++ scrapehistory.py
@@ -4,25 +4,43 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 import time
 
+urlList = []
 
 SEASON_MAX = 21
 season = 1
 
 driver = webdriver.Chrome()
+#wait for data to download
+driver.implicitly_wait(120)
+
+
+def getLocation(url):
+    driver.get(url)
+
+    location = driver.find_element(By.CLASS_NAME, "destination")
+    return location.text
 
 while season <= SEASON_MAX:
+
     URL = f"https://wherexur.com/{season}"
     driver.get(URL)
     
-    #wait for data to download
-    driver.implicitly_wait(120)
-    lists = driver.find_elements(By.CLASS_NAME, "list")
-    print(lists)
+    
+
     links = driver.find_elements(By.CSS_SELECTOR, "li.linked a")
     #print(links)
+
     for link in links:
-        print(link.get_attribute('href'))
-    
+        link = link.get_attribute('href').replace("https://wherexur.com/","")
+        if "item" in link:
+            continue
+        if "/" in link:
+            print(f"https://wherexur.com/{link}")
+            urlList.append(f"https://wherexur.com/{link}")
+
+
     season += 1
 
+print(urlList)
+print(urlList.sort())
 driver.quit()
