a26cf2092fbb7d27d1b78f52da5c81a97b8a34c6
lulamae12 <tls.tommy11@gmail.com>
2023-09-02 23:11:05 -0400

    commit backup data

--- data.py
+++ data.py
@@ -1,15 +1,19 @@
-#TOWER = 0
-
-#EDZ = 1
-
-#NESSUS = 2
+import matplotlib.pyplot as plt
+#data from DCV of Io, Titan etc after season 11:
 
 
+#0 == THE HANGAR 
+#1 == WINDING COVE
+#2 == WATCHER'S GRAVE
+
+#each item is a week starting 9/13/2020 corresponding to nums above
+locations = [0, 2, 0, 1, 0, 1, 0, 2, 1, 0, 1, 2, 1, 0, 1, 2, 1, 0, 2, 0, 2, 1, 0, 0, 0, 0, 2, 0, 1, 2, 0, 2, 1, 0, 2, 0, 2, 0, 2, 1, 0, 0, 2, 1, 0, 0, 2, 1, 0, 2, 0, 1, 2, 0, 2, 0, 2, 1, 0, 1, 0, 1, 2, 0, 1, 2, 1, 1, 0, 1, 2, 0, 2, 1, 0, 1, 2, 1, 1, 2, 0, 2, 0, 2, 0, 2, 1, 0, 2, 0, 1, 0, 2, 0, 1, 0, 2, 1, 0, 0, 2, 1, 2, 1, 2, 1, 0, 1, 1, 0, 2, 0, 2, 0, 1, 0, 1, 2, 0, 2, 1, 0, 1, 2, 1, 2, 0, 2, 1, 2, 1, 1, 0, 1, 2, 0, 1, 2, 0, 1, 0, 1, 2, 1, 2, 0, 1]
+xAx = []
+
+for i in range(len(locations)):
+    xAx.append(i)
 
 
-#locations starting from the week of 9/15/2017 at index 0
-locationData = [
-    2
-
-    
-]
+plt.plot(xAx, locations, '-')
+plt.scatter(xAx,locations)
+plt.show()
