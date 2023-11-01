0ba55ad5e736dc9b56ed5243ae641339b880c812
lulamae12 <tls.tommy11@gmail.com>
2023-11-01 16:02:42 -0400

    commit markov

--- tmp.py
+++ tmp.py
@@ -14,34 +14,48 @@ sequence = [
 ]
 
 #testing
-removeNWeeks = 0
+removeNWeeks = 2
 targetVal = None
 
 #remove last n items for testing
 for i in range(removeNWeeks):
     targetVal = sequence.pop()
 
+print("Last val: ",sequence[-1])
+# Order of the Markov Chain
+order = 1
+if len(sequence) < order:
+    raise ValueError("The sequence length must be greater than the order of the Markov Chain.")
+
 # Build the Markov Chain
 transition_matrix = defaultdict(lambda: defaultdict(int))
-for i in range(len(sequence) - 1):
-    curr_state, next_state = sequence[i], sequence[i+1]
-    transition_matrix[curr_state][next_state] += 1
+for i in range(len(sequence) - order):
+    curr_states = tuple(sequence[i:i+order])
+    next_state = sequence[i+order]
+    transition_matrix[curr_states][next_state] += 1
+
+k = 0.1  # Smaller than 1 to reduce the uniformity effect of smoothing
+
+
 
 # Convert counts to probabilities with smoothing
-V = len(set(sequence))  # Number of unique states
-for curr_state, transitions in transition_matrix.items():
+V = len(set(sequence))
+for curr_states, transitions in transition_matrix.items():
     total = sum(transitions.values())
     for possible_next_state in set(sequence):
         count = transitions[possible_next_state]
-        transition_matrix[curr_state][possible_next_state] = (count + 1) / (total + V)
+        transition_matrix[curr_states][possible_next_state] = (count + 1) / (total + V * k)
 
 # Predict the next item based on the current state and its transition probabilities
-curr_state = sequence[-1]
-predicted_probs = transition_matrix[curr_state]
+curr_states = tuple(sequence[-order:])
+predicted_probs = transition_matrix[curr_states]
 next_item = max(predicted_probs, key=predicted_probs.get)
 
 print("Predicted next item:", next_item)
 print("Target:", targetVal)
-print("\nTransition Probabilities from state", curr_state, ":")
+
+print(next_item == targetVal)
+
+print("\nTransition Probabilities from state", curr_states, ":")
 for state, prob in sorted(predicted_probs.items(), key=lambda x: x[1], reverse=True):
     print(f"Probability of transitioning to {state}: {prob:.4f}")
